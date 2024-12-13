5e64ec26480adaf7eff9b09de7d17eafe6b94875
Sally Huang <90699601+heyviehuang@users.noreply.github.com>
2024-12-13 11:04:18 +0800

    環境修正

--- src/text/greeting.ts
+++ src/text/greeting.ts
@@ -22,20 +22,20 @@ const replyToMessage = (ctx: Context, messageId: number, string: string) =>
 const replyWithImage = async (ctx: Context, messageId: number, imageUrl: string) => {
   try {
     // 使用 imageUrl 參數來發送圖片
-    const chatId = '-464385038'; // chat ID
+    const chatId = ctx.msg.chat.id;
     // 如果是 GIF 檔案，使用 sendAnimation 方法
     if (imageUrl.endsWith('.gif')) {
-    await bot.telegram.sendAnimation(chatId, imageUrl);
+      await bot.telegram.sendAnimation(chatId, imageUrl);
     } else {
-    // 否則使用 sendPhoto 方法發送靜態圖片
-    await bot.telegram.sendPhoto(chatId, imageUrl);
+      // 否則使用 sendPhoto 方法發送靜態圖片
+      await bot.telegram.sendPhoto(chatId, imageUrl);
     };
     console.log("Image sent successfully");
   } catch (error) {
     console.error("Failed to send image", error);
   }
 };
-  
+
 const greeting = () => async (ctx: Context) => {
   debug('Triggered "greeting" text command');
   // const now = moment().tz('Asia/Taipei');
@@ -66,6 +66,9 @@ const greeting = () => async (ctx: Context) => {
       { type: 'string', text: "甭客氣 能為您服務是我的榮幸" },
       { type: 'string', text: "瑞瑞一直都在❤" }
     ],
+    "孤單|寂寞": [
+      { type: 'string', text: "瑞瑞一直都在❤" }
+    ],
     "想下班|想回家": [
       { type: 'string', text: `${userName},去掃廁所!` }
     ],
@@ -77,10 +80,28 @@ const greeting = () => async (ctx: Context) => {
       { type: 'string', text: "100塊拿去買,記得打統編找錢,thx" }
     ],
     "下午茶|點心": [
-      { type: 'string', text: "我有買蛋糕,大家快來享用" }
+      { type: 'string', text: "我有買蛋糕,大家快來享用" },
+      { type: 'string', text: "今天下午茶是倒反天罡套餐~~" },
+      { type: 'string', text: "有人要來一杯五桐號嗎~~" },
+      { type: 'image', source: "https://cc.tvbs.com.tw/img/program/upload/2021/12/23/20211223155004-2a409f66.jpg" },//吐司
+      { type: 'string', text: "今天請大家喝手搖,幫我點最便宜的無糖紅茶,thx" },
+      { type: 'image', source: "https://i.imgur.com/vD727zv.jpeg" },//崩壞披薩
+      { type: 'image', source: "https://i.imgur.com/n6K8ZdO.jpeg" }//漢堡王
     ],
-    "大便|馬桶|廁所|髒|拉屎|屎|臭": [
-      { type: 'string', text: "馬桶上的尿垢再請Grace留意 齁齁" }
+    "飲料|口渴": [
+      // { type: 'string', text: "飲料太貴,喝水就好" },
+      // { type: 'string', text: "少無綠L,thx" },
+      { type: 'string', text: "Lerry：少無綠L,thx\nLeo：恆為濾水濾不乾淨地下水 Ｌ無糖\n馬哥：只能看不能喝全脂鮮乳 XS 全糖\nQAQQ ：尊榮無限續杯 全糖奶蓋 公升杯 X1" }
+    ],
+    "髒|臭": [
+      { type: 'string', text: "Grace去倒垃圾" },
+      { type: 'string', text: "Grace去掃廁所" },
+      { type: 'string', text: "廁所已拉封鎖線,敬請同仁見諒,thx" }
+    ],
+    "大便|馬桶|廁所|拉屎|拉粑粑": [
+      { type: 'string', text: "馬桶上的尿垢再請Grace留意 齁齁" },
+      { type: 'string', text: "我是廁所守門員 齁齁" },
+      { type: 'string', text: `${userName}還滿意我掃的廁所嗎?` },
     ],
     "需求|緊急|做不完|快爆炸|炸|技術": [
       { type: 'string', text: `${userName}將此項目列為最高priority,thx` }
@@ -97,8 +118,8 @@ const greeting = () => async (ctx: Context) => {
     ],
     "社團|校外教學": [
       { type: 'string', text: "這周社團活動是爬陽明山" },
-      { type: 'image', source: "https://i.imgur.com/00DD2XA.jpeg" },
-      { type: 'image', source: "https://i.imgur.com/uVuxvfm.jpeg" },
+      { type: 'image', source: "https://i.imgur.com/00DD2XA.jpeg" },//Sean單槓
+      { type: 'image', source: "https://i.imgur.com/uVuxvfm.jpeg" },//公司披薩
       { type: 'string', text: "這周社團活動是電影日,我幫大家訂了3個香菜豬血糕五更腸旺比薩" }
     ],
     "Grace|老妖": [
@@ -111,32 +132,46 @@ const greeting = () => async (ctx: Context) => {
     "垃圾": [
       { type: 'string', text: "廁所垃圾桶又滿了,誰是值日生?" }
     ],
+    "值日生": [
+      // { type: 'string', text: `今天的值日生是${getRandomName()}~~` },
+      { type: 'string', text: `今天的值日生是我~~` }
+    ],
     "開會|會議": [
-      { type: 'string', text: "我Main開投影機" }
+      { type: 'string', text: "我Main開投影機" },
+      { type: 'string', text: "該來會議室睡覺了" }
     ],
     "Gank": [
       { type: 'string', text: `${userName}進度如何?` }
     ],
     "想睡|賽車": [
-      { type: 'string', text: `${userName}請專心裝輪胎,thx` }
+      { type: 'string', text: `${userName}請專心裝輪胎,thx` },
+      { type: 'image', source: "https://i.imgur.com/ZmiQ73Y.jpeg" }//哀鳳阿罵
     ],
+    // "阿罵|啊罵|阿嬤|啊嬤|iphone|哀鳳": [
+    //   { type: 'image', source: "https://i.imgur.com/ZmiQ73Y.jpeg" },//哀鳳阿罵
+    //   { type: 'string', text: "Leo都說:愛情小坦克，撞誰誰休克" }
+    // ],
     "目標|團隊": [
       { type: 'string', text: "我們團隊就像f1賽車 後輪要裝在前輪上 輪胎要先洩氣,thx" }
-    ],  
+    ],
     "贏": [
-      { type: 'image', source: "https://i.imgur.com/VQsfguq.jpeg" }
+      { type: 'image', source: "https://i.imgur.com/VQsfguq.jpeg" }//Leo奪牌
     ],
     "薯條": [
       { type: 'string', text: "Shark不要偷吃Sean的薯條" }
     ],
     "累": [
-      { type: 'string', text: "請在跑輪裡盡情地選轉吧倉鼠" }
+      { type: 'string', text: "請在跑輪裡盡情地選轉吧倉鼠" },
+      { type: 'image', source: "https://i.imgur.com/ATtdvU8.gif" },//貓跌倒
     ],
     "下課|四點": [
       { type: 'string', text: `${userName}打羽球嗎?` }
     ],
-    "領導|主管|資方": [
-      { type: 'string', text: "魯哥 Kim 該來會議室檢討勞方了" }
+    "領導|主管|資方|管理|人事|員工": [
+      { type: 'string', text: "魯哥 Kim 該來會議室檢討勞方了" },
+      { type: 'string', text: "魯哥 Kim 該來會議室睡覺了" },
+      { type: 'image', source: "https://i.imgur.com/ATtdvU8.gif" },//貓跌倒
+      { type: 'string', text: "我好渴望有一套大公司的人事管理系統" },
     ],
     "本群": [
       { type: 'string', text: "動物生態評估" }
@@ -153,12 +188,16 @@ const greeting = () => async (ctx: Context) => {
     "咖啡": [
       { type: 'string', text: "Grace去洗咖啡機!" }
     ],
-    "考評|考績": [
-      { type: 'string', text: "公司嚴禁探問" }
+    "考評|考績|自評表": [
+      { type: 'string', text: "自評表請一律給自己C不能再高,thx" },
+      { type: 'string', text: `自評表${userName}寫再高,我也不會多給你錢,thx` }
     ],
     "獎金|年終": [
-      { type: 'image', source: "https://i.imgur.com/efLGupH.jpeg" },
+      { type: 'image', source: "https://i.imgur.com/efLGupH.jpeg" },//金錢是帶來快樂的梗圖
       { type: 'string', text: "輪胎都裝不好還想領錢" }
+    ],
+    "abtest|ab test": [
+      { type: 'string', text: "我在本公司提倡ABtest,結果只成QAtest" }
     ],
     "聚餐|餐聚": [
       { type: 'string', text: "旭集已訂位,thx" }
@@ -177,7 +216,8 @@ const greeting = () => async (ctx: Context) => {
       { type: 'string', text: "昨天誰最後走的,冷氣忘了關,請大家愛護北極熊,thx" }
     ],
     "遠端": [
-      { type: 'string', text: `就算${userName}你遠端在家,我也會密切關注你的進度` }
+      { type: 'string', text: `就算${userName}你遠端在家,我也會密切關注你的進度` },
+      { type: 'string', text: `我們應該要來推廣一下這個遠端連回去玩爐石的運動` }
     ],
     "打卡": [
       { type: 'string', text: `${userName} 快十點了,你刷臉了嗎?` }
@@ -198,14 +238,17 @@ const greeting = () => async (ctx: Context) => {
       { type: 'string', text: "泰文版的翻译进度需要评估" }
     ],
     "南港|爬山": [
-      { type: 'image', source: "https://i.imgur.com/00DD2XA.jpeg" },
+      { type: 'image', source: "https://i.imgur.com/00DD2XA.jpeg" },//Sean單槓
       { type: 'string', text: "南港山我用膝蓋在爬的" }
+    ],
+    "單槓": [
+      { type: 'image', source: "https://i.imgur.com/00DD2XA.jpeg" }//Sean單槓
     ],
     "福隆|腳踏車": [
       { type: 'string', text: "要來根烤香腸嗎~~" }
     ],
     "捕魚": [
-      { type: 'image', source: "https://i.imgur.com/KI7nE1b.gif" }
+      { type: 'image', source: "https://i.imgur.com/KI7nE1b.gif" }//加班飛彈
     ],
     "加班": [
       { type: 'image', source: "https://i.imgur.com/KI7nE1b.gif" },
@@ -215,9 +258,11 @@ const greeting = () => async (ctx: Context) => {
     "摳": [
       { type: 'string', text: `${userName}別這樣說,今天下午茶是星巴克` }
     ],
+    "可以打排球嗎": [
+      { type: 'string', text: `砸爛樓下大門我扛` }
+    ],
     "怕": [
-      { type: 'image', source: "https://i.imgur.com/BrA3Ug5.jpeg" },
-
+      { type: 'image', source: "https://i.imgur.com/BrA3Ug5.jpeg" },//怕爆
     ],
     "ok嗎?|可以嗎?": [
       { type: 'string', text: "1" }
@@ -226,14 +271,14 @@ const greeting = () => async (ctx: Context) => {
       { type: 'string', text: `${userName}找我嗎?` },
       { type: 'string', text: "齁齁" }
     ]
-  };  
+  };
 
 
   if (messageId) {
     for (const [keywords, responseOptions] of Object.entries(responses)) {
       if (text && new RegExp(`${keywords}`, 'i').test(text)) {  // 使用正則表達式來匹配關鍵字
-        
-      const response = responseOptions[Math.floor(Math.random() * responseOptions.length)];// 隨機選擇一個回覆選項
+
+        const response = responseOptions[Math.floor(Math.random() * responseOptions.length)];// 隨機選擇一個回覆選項
 
         if (typeof response === 'object' && response.type === 'image' && response.source) {
           // 回覆圖片
@@ -246,7 +291,7 @@ const greeting = () => async (ctx: Context) => {
       }
     }
   }
-  
+
   // greeting()--end
 }
 
@@ -261,30 +306,32 @@ const sendMessageToChat = async (chatId: number, message: string) => {
 };
 
 const schedules = [
-  { hour: 9, minute: 49, second: 0, message: '已消毒' },
-  { hour: 9, minute: 58, second: 0, message: '(開投影機)' },
-  { hour: 9, minute: 0, second: 0, message: '(大家起立)' },
-  { hour: 12, minute: 2, second: 30, message: `${getRandomName()}打卡了嗎?` },
-  { hour: 10, minute: 0, second: 30, message: '由RD先開始吧' },
-  { hour: 10, minute: 30, second: 0, message: `${getRandomName()}幫我開一下死人音樂,thx` },
-  { hour: 10, minute: 34, second: 0, message: '請兩位主管確實回報消毒,thx' },
-  { hour: 10, minute: 40, second: 0, message: '公司還是希望大家生活品質要兼顧,絕對不是說加班費公司付不起' },
-  { hour: 11, minute: 30, second: 0, message: '各位,電視我已經砸了,以後請戴耳機聽H5版KKBOX,thx' },
+  // { hour: 9, minute: 49, second: 0, message: '已消毒' },
+  // { hour: 9, minute: 58, second: 0, message: '(開投影機)' },
+  // { hour: 10, minute: 0, second: 0, message: '(大家起立)' },
+  // { hour: 10, minute: 5, second: 0, message: '今天這個天氣,我想給自己放個假,在家睡覺' },
+  // { hour: 10, minute: 2, second: 30, message: `${getRandomName()}打卡了嗎?` },
+  // { hour: 10, minute: 0, second: 30, message: '由RD先開始吧' },
+  // { hour: 10, minute: 30, second: 0, message: `${getRandomName()}幫我開一下死人音樂,thx` },
+  // { hour: 10, minute: 34, second: 0, message: '請兩位主管確實回報消毒,thx' },
+  // { hour: 10, minute: 40, second: 0, message: '公司還是希望大家生活品質要兼顧,絕對不是說加班費公司付不起' },
+  // { hour: 11, minute: 30, second: 0, message: '各位,電視我已經砸了,以後請戴耳機聽H5版KKBOX,thx' },
   { hour: 12, minute: 1, second: 0, message: `${getRandomName()}中午吃什麼?` },
-  { hour: 13, minute: 35, second: 0, message: 'Apple燈幫我開一下,thx' },
-  { hour: 14, minute: 34, second: 0, message: '廁所衛生紙沒了,值日生是誰?' },
-  { hour: 14, minute: 53, second: 0, message: '禮拜三早上週會討論,thx' },
-  { hour: 15, minute: 27, second: 0, message: '富翔,你過來一下' },
-  { hour: 16, minute: 1, second: 0, message: `${getRandomName()}打羽球嗎?` },
-  { hour: 16, minute: 12, second: 0, message: 'Ken哥你再抽要QAQQ了' },
-  { hour: 16, minute: 33, second: 0, message: 'Devin這週分享會要分享什麼呢?' },
-  { hour: 16, minute: 49, second: 0, message: 'Una別再睡了~~' },
-  { hour: 16, minute: 52, second: 0, message: 'Grace花快被妳澆死了' },
-  { hour: 17, minute: 24, second: 0, message: '各位可以開始清滾輪囉~~清完沒有獎勵' },
-  { hour: 17, minute: 30, second: 30, message: '倉鼠們收拾書包，踮起腳尖往停車場移動！' },
-  { hour: 17, minute: 31, second: 0, message: '公園美景第一排，我先去佔位了' },
-  { hour: 18, minute: 0, second: 0, message: 'QAQQ' },
-  { hour: 18, minute: 30, second: 0, message: '魯哥,要下班了嗎?' },
+  { hour: 10, minute: 38, second: 50, message: `Sean,晚点请过来一下会议室,thx` },
+  // { hour: 13, minute: 35, second: 0, message: 'Apple幫我關燈我要睡覺thx' },
+  // { hour: 14, minute: 34, second: 0, message: '廁所衛生紙沒了,值日生是誰?' },
+  // { hour: 14, minute: 53, second: 0, message: '禮拜三早上週會討論,thx' },
+  // { hour: 15, minute: 27, second: 0, message: '富翔,你過來一下' },
+  // { hour: 16, minute: 1, second: 0, message: `${getRandomName()}四點了,放風啦~~` },
+  // { hour: 16, minute: 12, second: 0, message: 'Ken哥你再抽要QAQQ了' },
+  // { hour: 16, minute: 33, second: 0, message: 'Devin這週分享會要分享什麼呢?' },
+  // { hour: 16, minute: 49, second: 0, message: 'Una別再睡了~~' },
+  // { hour: 16, minute: 52, second: 0, message: 'Grace花快被妳澆死了' },
+  // { hour: 17, minute: 24, second: 0, message: '各位可以開始清滾輪囉~~清完沒有獎勵' },
+  // { hour: 17, minute: 30, second: 30, message: '倉鼠們收拾書包，踮起腳尖往停車場移動！' },
+  // { hour: 17, minute: 31, second: 0, message: '公園美景第一排，我先去佔位了' },
+  // { hour: 18, minute: 0, second: 0, message: 'QAQQ' },
+  { hour: 18, minute: 30, second: 0, message: '魯哥,要下班了嗎?' }
 ];
 
 // 排程發送訊息
@@ -298,4 +345,4 @@ schedules.forEach((scheduleItem) => {
 
 // bot.command('greeting', greeting()); 
 // bot.launch(); 
-export { greeting,sendMessageToChat,replyWithImage };
+export { greeting, sendMessageToChat, replyWithImage };
